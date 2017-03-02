95262d13afacc83219a5d34ae7b07ea8547df6c5
Lassi Kortela <lassi@lassi.io>
2017-03-02 13:02:39 +0200

    Add demo
    Parses CSV files (S-Pankki -> Tiliote -> PDF -> Tabula -> CSV -> Demo
    -> console.log)

---
+++ demo.js
@@ -0,0 +1,28 @@
+$(function() {
+
+    var fileSelect = document.getElementById("fileSelect");
+    var fileElem = document.getElementById("fileElem");
+
+    function handleFiles(files) {
+        for (var i = 0; i < files.length; i++) {
+            var file = files[i];
+            if (!/^text\//.test(file.type))
+                continue;
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                console.log(Pankkiparseri.parseSPankkiCSV(e.target.result));
+            }
+            reader.readAsText(file);
+        }
+    }
+
+    fileElem.addEventListener("change", function (e) {
+        handleFiles(this.files);
+    });
+
+    fileSelect.addEventListener("click", function (e) {
+        fileElem.click();
+        e.preventDefault(); // prevent navigation to "#"
+    }, false);
+
+});
