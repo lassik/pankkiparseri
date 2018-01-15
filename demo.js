3ab685a8626b76007eb9a2cbfa9bf95e94a01775
Lassi Kortela <lassi@lassi.io>
2018-01-15 04:03:20 +0200

    Reformat
    Emacs js-format: js-format-buffer standard

--- demo.js
+++ demo.js
@@ -1,30 +1,27 @@
-$(function() {
+$(function () {
+    var fileSelect = document.getElementById('fileSelect')
+    var fileElem = document.getElementById('fileElem')
 
-    var fileSelect = document.getElementById("fileSelect");
-    var fileElem = document.getElementById("fileElem");
-
-    function handleFiles(files) {
-        for (var i = 0; i < files.length; i++) {
-            var file = files[i];
-            if (!/^text\//.test(file.type))
-                continue;
-            var reader = new FileReader();
-	    var parse;
-	    parse = Pankkiparseri.parseSPankkiCSV;
-            reader.onload = function(e) {
-                console.log(parse(e.target.result));
-            }
-            reader.readAsText(file);
-        }
+    function handleFiles (files) {
+	for (var i = 0; i < files.length; i++) {
+	    var file = files[i]
+	    if (!/^text\//.test(file.type)) { continue }
+	    var reader = new FileReader()
+	    var parse
+	    parse = Pankkiparseri.parseSPankkiCSV
+	    reader.onload = function (e) {
+		console.log(parse(e.target.result))
+	    }
+	    reader.readAsText(file)
+	}
     }
 
-    fileElem.addEventListener("change", function (e) {
-        handleFiles(this.files);
-    });
+    fileElem.addEventListener('change', function (e) {
+	handleFiles(this.files)
+    })
 
-    fileSelect.addEventListener("click", function (e) {
-        fileElem.click();
-        e.preventDefault(); // prevent navigation to "#"
-    }, false);
-
-});
+    fileSelect.addEventListener('click', function (e) {
+	fileElem.click()
+	e.preventDefault() // prevent navigation to "#"
+    }, false)
+})
