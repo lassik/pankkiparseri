b79a7bdad596f0c3b898443418c15ddc0b59ecf0
Lassi Kortela <lassi@lassi.io>
2018-01-15 04:26:26 +0200

    Refactor
    Extract date and amount functions from main parser

--- pankkiparseri.js
+++ pankkiparseri.js
@@ -1,30 +1,43 @@
 Pankkiparseri = {}
 
+Pankkiparseri.isoDateFromFinnishDate = function (finnishDate) {
+    var g = finnishDate.match(/^(\d{1,2})\.(\d{1,2})\.(\d{2,4})$/)
+    if (!g) {
+        return null
+    }
+    var year = (g[3].length === 2) ? ('20' + g[3]) : g[3]
+    var month = g[2].padStart(2, '0')
+    var day = g[1].padStart(2, '0')
+    return year + '-' + month + '-' + day
+}
+
+Pankkiparseri.centsFromAmountSignLast = function (formattedAmount) {
+    var g = formattedAmount.match(/^(\d{1,5}),(\d{2})([+-])$/)
+    if (!g) {
+        return null
+    }
+    var euros = parseInt(g[1])
+    var cents = parseInt(g[2])
+    var sign = parseInt(g[3] + '1')
+    return sign * ((100 * euros) + cents)
+}
+
 Pankkiparseri.parseSPankkiTilioteCSV = function (contents) {
     // CSV ripped from S-Pankki bank statement PDF by Tabula.
     var rows = $.csv.toArrays(contents)
     var entries = []
     var date = null
-    var m
+    var g
     for (var lineNum = 1; lineNum <= rows.length; lineNum++) {
         var row = rows[lineNum - 1]
-        if ((m = row[0].match(/^KIRJAUSPÄIVÄ (\d{2})\.(\d{2})\.(\d{2})/))) {
-            var day = m[1]
-            var month = m[2]
-            var year = '20' + m[3]
-            date = year + '-' + month + '-' + day
+        if ((g = row[0].match(/^KIRJAUSPÄIVÄ (\d{2}\.\d{2}\.\d{2})/))) {
+            date = Pankkiparseri.isoDateFromFinnishDate(g[1])
         } else if (row[0].match(/^\d{18} [A-Z]/)) {
             var entry = {}
             entry.date = date
             entry.archivalId = row[0]
             entry.otherParty = row[1]
-            var formattedAmount = row[row.length - 1]
-            if ((m = formattedAmount.match(/^(\d{1,5}),(\d{2})([+-])$/))) {
-                entry.cents = (100 * parseInt(m[1]) + parseInt(m[2])) *
-                    parseInt(m[3] + '1')
-            } else {
-                entry.cents = null
-            }
+            entry.cents = Pankkiparseri.centsFromAmountSignLast(row[row.length - 1])
             entries.push(entry)
         }
     }
