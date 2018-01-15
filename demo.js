82e73da3699bc326d7034e08fe8816232e1a3d4c
Lassi Kortela <lassi@lassi.io>
2018-01-15 04:08:03 +0200

    Untabify!

--- demo.js
+++ demo.js
@@ -3,25 +3,25 @@ $(function () {
     var fileElem = document.getElementById('fileElem')
 
     function handleFiles (files) {
-	for (var i = 0; i < files.length; i++) {
-	    var file = files[i]
-	    if (!/^text\//.test(file.type)) { continue }
-	    var reader = new FileReader()
-	    var parse
-	    parse = Pankkiparseri.parseSPankkiTilioteCSV
-	    reader.onload = function (e) {
-		console.log(parse(e.target.result))
-	    }
-	    reader.readAsText(file)
-	}
+        for (var i = 0; i < files.length; i++) {
+            var file = files[i]
+            if (!/^text\//.test(file.type)) { continue }
+            var reader = new FileReader()
+            var parse
+            parse = Pankkiparseri.parseSPankkiTilioteCSV
+            reader.onload = function (e) {
+                console.log(parse(e.target.result))
+            }
+            reader.readAsText(file)
+        }
     }
 
     fileElem.addEventListener('change', function (e) {
-	handleFiles(this.files)
+        handleFiles(this.files)
     })
 
     fileSelect.addEventListener('click', function (e) {
-	fileElem.click()
-	e.preventDefault() // prevent navigation to "#"
+        fileElem.click()
+        e.preventDefault() // prevent navigation to "#"
     }, false)
 })
--- pankkiparseri.js
+++ pankkiparseri.js
@@ -7,26 +7,26 @@ Pankkiparseri.parseSPankkiTilioteCSV = function (contents) {
     var date = null
     var m
     for (var lineNum = 1; lineNum <= rows.length; lineNum++) {
-	var row = rows[lineNum - 1]
-	if ((m = row[0].match(/^KIRJAUSPÄIVÄ (\d{2})\.(\d{2})\.(\d{2})/))) {
-	    var day = m[1]
-	    var month = m[2]
-	    var year = '20' + m[3]
-	    date = year + '-' + month + '-' + day
-	} else if (row[0].match(/^\d{18} [A-Z]/)) {
-	    var entry = {}
-	    entry.date = date
-	    entry.archivalId = row[0]
-	    entry.otherParty = row[1]
-	    var formattedAmount = row[row.length - 1]
-	    if ((m = formattedAmount.match(/^(\d{1,5}),(\d{2})([+-])$/))) {
-		entry.cents = (100 * parseInt(m[1]) + parseInt(m[2])) *
-		    parseInt(m[3] + '1')
-	    } else {
-		entry.cents = null
-	    }
-	    entries.push(entry)
-	}
+        var row = rows[lineNum - 1]
+        if ((m = row[0].match(/^KIRJAUSPÄIVÄ (\d{2})\.(\d{2})\.(\d{2})/))) {
+            var day = m[1]
+            var month = m[2]
+            var year = '20' + m[3]
+            date = year + '-' + month + '-' + day
+        } else if (row[0].match(/^\d{18} [A-Z]/)) {
+            var entry = {}
+            entry.date = date
+            entry.archivalId = row[0]
+            entry.otherParty = row[1]
+            var formattedAmount = row[row.length - 1]
+            if ((m = formattedAmount.match(/^(\d{1,5}),(\d{2})([+-])$/))) {
+                entry.cents = (100 * parseInt(m[1]) + parseInt(m[2])) *
+                    parseInt(m[3] + '1')
+            } else {
+                entry.cents = null
+            }
+            entries.push(entry)
+        }
     }
     return entries
 }
