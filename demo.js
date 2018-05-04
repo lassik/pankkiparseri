61802492fd8d46291df117d2cd34e49ee227a62b
Lassi Kortela <lassi@lassi.io>
2018-05-04 23:35:54 +0300

    Reformat using 'prettier'

--- demo.js
+++ demo.js
@@ -1,45 +1,57 @@
-$(function () {
-    var allEntries = []
+$(function() {
+  var allEntries = [];
 
-    function downloadXmlFile (filename, xmlString) {
-        var base64 = btoa(unescape(encodeURIComponent(xmlString)))
-        var hiddenLink = document.createElement('a')
-        hiddenLink.download = filename
-        hiddenLink.href = 'data:text/xml;charset=utf-8;base64,' + base64
-        document.body.appendChild(hiddenLink)
-        hiddenLink.click()
-        document.body.removeChild(hiddenLink)
-    }
+  function downloadXmlFile(filename, xmlString) {
+    var base64 = btoa(unescape(encodeURIComponent(xmlString)));
+    var hiddenLink = document.createElement("a");
+    hiddenLink.download = filename;
+    hiddenLink.href = "data:text/xml;charset=utf-8;base64," + base64;
+    document.body.appendChild(hiddenLink);
+    hiddenLink.click();
+    document.body.removeChild(hiddenLink);
+  }
 
-    function downloadOfxFile () {
-        downloadXmlFile('pankkiparseri.ofx',
-                        Pankkiparseri.ofxStringFromEntries(allEntries))
-    }
+  function downloadOfxFile() {
+    downloadXmlFile(
+      "pankkiparseri.ofx",
+      Pankkiparseri.ofxStringFromEntries(allEntries)
+    );
+  }
 
-    function updateEntriesTable () {
-        $('#entries').empty()
-        $('#entries').append(
-            $('<tr/>')
-                .append($('<th/>').text('Pvm'))
-                .append($('<th/>').text('Määrä'))
-                .append($('<th/>').text('Kuka'))
-                .append($('<th/>').text('Viesti')))
-        allEntries.forEach(function (entry) {
-            $('#entries').append(
-                $('<tr/>')
-                    .append($('<td/>').text(entry.date.iso))
-                    .append($('<td/>').text(entry.amount ? entry.amount.eurosCommaCents+' €' : '').addClass('right'))
-                    .append($('<td/>').text(entry.otherParty))
-                    .append($('<td/>').text(entry.message)))
-        })
-    }
+  function updateEntriesTable() {
+    $("#entries").empty();
+    $("#entries").append(
+      $("<tr/>")
+        .append($("<th/>").text("Pvm"))
+        .append($("<th/>").text("Määrä"))
+        .append($("<th/>").text("Kuka"))
+        .append($("<th/>").text("Viesti"))
+    );
+    allEntries.forEach(function(entry) {
+      $("#entries").append(
+        $("<tr/>")
+          .append($("<td/>").text(entry.date.iso))
+          .append(
+            $("<td/>")
+              .text(entry.amount ? entry.amount.eurosCommaCents + " €" : "")
+              .addClass("right")
+          )
+          .append($("<td/>").text(entry.otherParty))
+          .append($("<td/>").text(entry.message))
+      );
+    });
+  }
 
-    function addEntries (newEntries) {
-        newEntries.forEach(function (entry) { allEntries.push(entry) })
-        allEntries.sort(function (a, b) { return a.date.iso.localeCompare(b.date.iso) })
-        updateEntriesTable()
-    }
+  function addEntries(newEntries) {
+    newEntries.forEach(function(entry) {
+      allEntries.push(entry);
+    });
+    allEntries.sort(function(a, b) {
+      return a.date.iso.localeCompare(b.date.iso);
+    });
+    updateEntriesTable();
+  }
 
-    Pankkiparseri.addToForm('pankki', addEntries)
-    $('#downloadButton').click(downloadOfxFile)
-})
+  Pankkiparseri.addToForm("pankki", addEntries);
+  $("#downloadButton").click(downloadOfxFile);
+});
--- pankkiparseri.js
+++ pankkiparseri.js
@@ -1,173 +1,191 @@
-Pankkiparseri = {}
+Pankkiparseri = {};
 
-Pankkiparseri.withoutLeadingDayAndMonth = function (s) {
-    var g = s.match(/^[0-3]\d[0-1]\d (.*)$/)
-    return g ? g[1] : s
-}
+Pankkiparseri.withoutLeadingDayAndMonth = function(s) {
+  var g = s.match(/^[0-3]\d[0-1]\d (.*)$/);
+  return g ? g[1] : s;
+};
 
-Pankkiparseri.parseFinnishDate = function (finnishDate) {
-    var g = finnishDate.match(/^(\d{1,2})\.(\d{1,2})\.(\d{2,4})$/)
-    if (!g) {
-        return null
+Pankkiparseri.parseFinnishDate = function(finnishDate) {
+  var g = finnishDate.match(/^(\d{1,2})\.(\d{1,2})\.(\d{2,4})$/);
+  if (!g) {
+    return null;
+  }
+  var year = g[3].length === 2 ? "20" + g[3] : g[3];
+  var month = g[2].padStart(2, "0");
+  var day = g[1].padStart(2, "0");
+  return {
+    iso: year + "-" + month + "-" + day,
+    isoNoDashes: year + month + day
+  };
+};
+
+Pankkiparseri.parseAmountPreparsed = function(eurosStr, commaCents, signStr) {
+  var euros = parseInt(eurosStr.replace(/\./g, ""));
+  eurosStr = String(euros);
+  var centsStr = (commaCents ? commaCents.slice(1) : "0").padEnd(2, "0");
+  var cents = parseInt(centsStr);
+  var sign = signStr === "-" ? -1 : 1;
+  signStr = sign < 0 ? "-" : "+";
+  return {
+    cents: sign * (100 * euros + cents),
+    eurosDotCents: signStr + eurosStr + "." + centsStr,
+    eurosCommaCents: signStr + eurosStr + "," + centsStr
+  };
+};
+
+Pankkiparseri.parseAmountSignFirst = function(formattedAmount) {
+  var g = formattedAmount.match(/^([+-]?)([\d.]{1,5})(,\d{1,2})?$/);
+  return g ? Pankkiparseri.parseAmountPreparsed(g[2], g[3], g[1]) : null;
+};
+
+Pankkiparseri.parseAmountSignLast = function(formattedAmount) {
+  var g = formattedAmount.match(/^([\d.]{1,5})(,\d{1,2})?([+-?])$/);
+  return g ? Pankkiparseri.parseAmountPreparsed(g[1], g[2], g[3]) : null;
+};
+
+Pankkiparseri.parseOmaSaastopankkiTilitapahtumatCSV = function(contents) {
+  // Official CSV download from online banking "tilitapahtumat" section.
+  var rows = $.csv.toArrays(contents, { separator: ";" });
+  var entries = [];
+  for (var lineNum = 2; lineNum <= rows.length; lineNum++) {
+    var row = rows[lineNum - 1];
+    var entry = {};
+    entry.date = Pankkiparseri.parseFinnishDate(row[0]);
+    entry.otherParty = row[1];
+    entry.message = row[3].match(/^'/) ? row[3].slice(1) : "";
+    entry.amount = Pankkiparseri.parseAmountSignFirst(row[4]);
+    entries.push(entry);
+  }
+  return entries;
+};
+
+Pankkiparseri.parseSPankkiTilioteTabulaCSV = function(contents) {
+  // CSV ripped from S-Pankki bank statement PDF by Tabula with autodetect.
+  var rows = $.csv.toArrays(contents);
+  var entries = [];
+  var entry = null;
+  var sinceEntry = null;
+  var date = null;
+  var g;
+  for (var lineNum = 1; lineNum <= rows.length; lineNum++) {
+    var row = rows[lineNum - 1];
+    if ((g = row[0].match(/^KIRJAUSPÄIVÄ (\d{2}\.\d{2}\.\d{2})/))) {
+      date = Pankkiparseri.parseFinnishDate(g[1]);
+      entry = sinceEntry = null;
+    } else if (row[0].match(/^\d{18}( [A-Z])?/)) {
+      sinceEntry = 0;
+      entry = {};
+      entry.date = date;
+      entry.archivalId = row[0];
+      entry.otherParty = Pankkiparseri.withoutLeadingDayAndMonth(row[1]);
+      entry.amount = Pankkiparseri.parseAmountSignLast(row[row.length - 1]);
+      entry.message = "";
+      entries.push(entry);
+    } else if (sinceEntry === 1 || sinceEntry === 2) {
+      if (entry.message.length > 0) {
+        entry.message += " ";
+      }
+      entry.message += Pankkiparseri.withoutLeadingDayAndMonth(row[1]);
     }
-    var year = (g[3].length === 2) ? ('20' + g[3]) : g[3]
-    var month = g[2].padStart(2, '0')
-    var day = g[1].padStart(2, '0')
-    return {
-        iso: year + '-' + month + '-' + day,
-        isoNoDashes: year + month + day
+    if (sinceEntry !== null) {
+      sinceEntry += 1;
     }
-}
+  }
+  return entries;
+};
 
-Pankkiparseri.parseAmountPreparsed = function (eurosStr, commaCents, signStr) {
-    var euros = parseInt(eurosStr.replace(/\./g, ''))
-    eurosStr = String(euros)
-    var centsStr = (commaCents ? commaCents.slice(1) : '0').padEnd(2, '0')
-    var cents = parseInt(centsStr)
-    var sign = (signStr === '-') ? -1 : 1
-    signStr = (sign < 0) ? '-' : '+'
-    return {
-        cents: sign * ((100 * euros) + cents),
-        eurosDotCents: signStr + eurosStr + '.' + centsStr,
-        eurosCommaCents: signStr + eurosStr + ',' + centsStr
+Pankkiparseri.handleFiles = function(files, entriesCallback, parse, encoding) {
+  for (var i = 0; i < files.length; i++) {
+    var file = files[i];
+    if (!/^text\//.test(file.type)) {
+      continue;
     }
-}
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      entriesCallback(parse(e.target.result));
+    };
+    reader.readAsText(file, encoding);
+  }
+};
 
-Pankkiparseri.parseAmountSignFirst = function (formattedAmount) {
-    var g = formattedAmount.match(/^([+-]?)([\d.]{1,5})(,\d{1,2})?$/)
-    return g ? Pankkiparseri.parseAmountPreparsed(g[2], g[3], g[1]) : null
-}
+Pankkiparseri.addBankToForm = function(
+  form,
+  entriesCallback,
+  bankTitle,
+  parse,
+  encoding
+) {
+  var hiddenInput = document.createElement("input");
+  hiddenInput.style.display = "none";
+  hiddenInput.type = "file";
+  hiddenInput.accept = ".csv,text/csv";
+  hiddenInput.multiple = true;
+  hiddenInput.addEventListener("change", function(e) {
+    Pankkiparseri.handleFiles(this.files, entriesCallback, parse, encoding);
+  });
+  form.appendChild(hiddenInput);
+  var button = document.createElement("button");
+  button.appendChild(document.createTextNode(bankTitle));
+  button.addEventListener(
+    "click",
+    function(e) {
+      hiddenInput.click();
+      e.preventDefault(); // prevent navigation to "#"
+    },
+    false
+  );
+  form.appendChild(button);
+};
 
-Pankkiparseri.parseAmountSignLast = function (formattedAmount) {
-    var g = formattedAmount.match(/^([\d.]{1,5})(,\d{1,2})?([+-?])$/)
-    return g ? Pankkiparseri.parseAmountPreparsed(g[1], g[2], g[3]) : null
-}
+Pankkiparseri.addToForm = function(formId, entriesCallback) {
+  var form = document.getElementById(formId);
+  Pankkiparseri.addBankToForm(
+    form,
+    entriesCallback,
+    "S-Pankki (tiliote Tabula CSV)",
+    Pankkiparseri.parseSPankkiTilioteTabulaCSV,
+    "UTF-8"
+  );
+  Pankkiparseri.addBankToForm(
+    form,
+    entriesCallback,
+    "Oma Säästöpankki (tilitapahtumat CSV)",
+    Pankkiparseri.parseOmaSaastopankkiTilitapahtumatCSV,
+    "ISO-8859-15"
+  );
+};
 
-Pankkiparseri.parseOmaSaastopankkiTilitapahtumatCSV = function (contents) {
-    // Official CSV download from online banking "tilitapahtumat" section.
-    var rows = $.csv.toArrays(contents, {separator: ';'})
-    var entries = []
-    for (var lineNum = 2; lineNum <= rows.length; lineNum++) {
-        var row = rows[lineNum - 1]
-        var entry = {}
-        entry.date = Pankkiparseri.parseFinnishDate(row[0])
-        entry.otherParty = row[1]
-        entry.message = (row[3].match(/^'/) ? row[3].slice(1) : '')
-        entry.amount = Pankkiparseri.parseAmountSignFirst(row[4])
-        entries.push(entry)
-    }
-    return entries
-}
-
-Pankkiparseri.parseSPankkiTilioteTabulaCSV = function (contents) {
-    // CSV ripped from S-Pankki bank statement PDF by Tabula with autodetect.
-    var rows = $.csv.toArrays(contents)
-    var entries = []
-    var entry = null
-    var sinceEntry = null
-    var date = null
-    var g
-    for (var lineNum = 1; lineNum <= rows.length; lineNum++) {
-        var row = rows[lineNum - 1]
-        if ((g = row[0].match(/^KIRJAUSPÄIVÄ (\d{2}\.\d{2}\.\d{2})/))) {
-            date = Pankkiparseri.parseFinnishDate(g[1])
-            entry = sinceEntry = null
-        } else if (row[0].match(/^\d{18}( [A-Z])?/)) {
-            sinceEntry = 0
-            entry = {}
-            entry.date = date
-            entry.archivalId = row[0]
-            entry.otherParty = Pankkiparseri.withoutLeadingDayAndMonth(row[1])
-            entry.amount = Pankkiparseri.parseAmountSignLast(row[row.length - 1])
-            entry.message = ''
-            entries.push(entry)
-        } else if ((sinceEntry === 1 || sinceEntry === 2)) {
-            if (entry.message.length > 0) {
-                entry.message += ' '
-            }
-            entry.message += Pankkiparseri.withoutLeadingDayAndMonth(row[1])
-        }
-        if (sinceEntry !== null) {
-            sinceEntry += 1
-        }
-    }
-    return entries
-}
-
-Pankkiparseri.handleFiles = function (files, entriesCallback, parse, encoding) {
-    for (var i = 0; i < files.length; i++) {
-        var file = files[i]
-        if (!/^text\//.test(file.type)) { continue }
-        var reader = new FileReader()
-        reader.onload = function (e) {
-            entriesCallback(parse(e.target.result))
-        }
-        reader.readAsText(file, encoding)
-    }
-}
-
-Pankkiparseri.addBankToForm = function (form, entriesCallback, bankTitle, parse, encoding) {
-    var hiddenInput = document.createElement('input')
-    hiddenInput.style.display = 'none'
-    hiddenInput.type = 'file'
-    hiddenInput.accept = '.csv,text/csv'
-    hiddenInput.multiple = true
-    hiddenInput.addEventListener('change', function (e) {
-        Pankkiparseri.handleFiles(this.files, entriesCallback, parse, encoding)
-    })
-    form.appendChild(hiddenInput)
-    var button = document.createElement('button')
-    button.appendChild(document.createTextNode(bankTitle))
-    button.addEventListener('click', function (e) {
-        hiddenInput.click()
-        e.preventDefault() // prevent navigation to "#"
-    }, false)
-    form.appendChild(button)
-}
-
-Pankkiparseri.addToForm = function (formId, entriesCallback) {
-    var form = document.getElementById(formId)
-    Pankkiparseri.addBankToForm(
-        form, entriesCallback,
-        'S-Pankki (tiliote Tabula CSV)',
-        Pankkiparseri.parseSPankkiTilioteTabulaCSV, 'UTF-8')
-    Pankkiparseri.addBankToForm(
-        form, entriesCallback,
-        'Oma Säästöpankki (tilitapahtumat CSV)',
-        Pankkiparseri.parseOmaSaastopankkiTilitapahtumatCSV, 'ISO-8859-15')
-}
-
-Pankkiparseri.ofxStringFromEntries = function (entries) {
-    var doc
-    function elem (tag, bodyText) {
-        var e = doc.createElement(tag)
-        e.append(String(bodyText))
-        return e
-    }
-    doc = document.implementation.createDocument(null, 'OFX')
-    var bankmsgsrsv1 = doc.createElement('BANKMSGSRSV1')
-    doc.documentElement.appendChild(bankmsgsrsv1)
-    var stmttrnrs = doc.createElement('STMTTRNRS')
-    bankmsgsrsv1.appendChild(stmttrnrs)
-    var stmtrs = doc.createElement('STMTRS')
-    stmttrnrs.appendChild(stmtrs)
-    var bankacctfrom = doc.createElement('BANKACCTFROM')
-    bankacctfrom.appendChild(elem('BANKID', '000000000'))
-    bankacctfrom.appendChild(elem('ACCTID', '0000000'))
-    bankacctfrom.appendChild(elem('ACCTTYPE', 'CHECKING'))
-    stmtrs.appendChild(bankacctfrom)
-    var banktranlist = doc.createElement('BANKTRANLIST')
-    var i = 0
-    entries.forEach(function (entry) {
-        var stmttrn = doc.createElement('STMTTRN')
-        stmttrn.appendChild(elem('FITID', i))
-        stmttrn.appendChild(elem('TRNTYPE', 'CHECK'))
-        stmttrn.appendChild(elem('DTPOSTED', entry.date.isoNoDashes))
-        stmttrn.appendChild(elem('TRNAMT', entry.amount.eurosDotCents))
-        stmttrn.appendChild(elem('MEMO', entry.message))
-        banktranlist.appendChild(stmttrn)
-        i++
-    })
-    stmtrs.appendChild(banktranlist)
-    return (new XMLSerializer()).serializeToString(doc)
-}
+Pankkiparseri.ofxStringFromEntries = function(entries) {
+  var doc;
+  function elem(tag, bodyText) {
+    var e = doc.createElement(tag);
+    e.append(String(bodyText));
+    return e;
+  }
+  doc = document.implementation.createDocument(null, "OFX");
+  var bankmsgsrsv1 = doc.createElement("BANKMSGSRSV1");
+  doc.documentElement.appendChild(bankmsgsrsv1);
+  var stmttrnrs = doc.createElement("STMTTRNRS");
+  bankmsgsrsv1.appendChild(stmttrnrs);
+  var stmtrs = doc.createElement("STMTRS");
+  stmttrnrs.appendChild(stmtrs);
+  var bankacctfrom = doc.createElement("BANKACCTFROM");
+  bankacctfrom.appendChild(elem("BANKID", "000000000"));
+  bankacctfrom.appendChild(elem("ACCTID", "0000000"));
+  bankacctfrom.appendChild(elem("ACCTTYPE", "CHECKING"));
+  stmtrs.appendChild(bankacctfrom);
+  var banktranlist = doc.createElement("BANKTRANLIST");
+  var i = 0;
+  entries.forEach(function(entry) {
+    var stmttrn = doc.createElement("STMTTRN");
+    stmttrn.appendChild(elem("FITID", i));
+    stmttrn.appendChild(elem("TRNTYPE", "CHECK"));
+    stmttrn.appendChild(elem("DTPOSTED", entry.date.isoNoDashes));
+    stmttrn.appendChild(elem("TRNAMT", entry.amount.eurosDotCents));
+    stmttrn.appendChild(elem("MEMO", entry.message));
+    banktranlist.appendChild(stmttrn);
+    i++;
+  });
+  stmtrs.appendChild(banktranlist);
+  return new XMLSerializer().serializeToString(doc);
+};
